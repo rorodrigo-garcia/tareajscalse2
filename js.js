301c1581c67d7be3020a1e2b507efde532ab6b2f
Rodrigar <rodriadriangarcia1997@gmail.com>
2022-08-29 23:47:35 -0300

    terminada la tarea

---
+++ js.js
@@ -0,0 +1,5 @@
+let pregunta = prompt ("Escriba el personaje de Marvel conocido que levanto el martillo de Thor");
+while (pregunta != "Capitan America" ) {
+alert("El usuario ingreso " + pregunta);
+pregunta = prompt("Ese no,siga intentando")
+}
